c208a340046c9d2fb1019b066ed16983b34634db
PRASANN SONI <prasannsoni@PRASANNs-MacBook-Air.local>
2026-03-20 18:33:02 +0530

    added jee page, script and styling updates

---
+++ todo/script.js
@@ -0,0 +1,9 @@
+var jee = document.querySelector('.jee');
+var neet = document.querySelector('.neet');
+
+jee.addEventListener('click',()=>{
+    window.location.href = "jee.html";
+})
+neet.addEventListener('click',()=>{
+    window.location.href = "index.html";
+})
